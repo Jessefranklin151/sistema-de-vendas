46c811238f17dd7869c71b108b94383d4ce4faf7
jesse <jesseoito@gmail.com>
2018-10-13 14:25:16 -0300

    tirando um import

--- src/main/java/sistemaDeVendas/services/PedidoService.java
+++ src/main/java/sistemaDeVendas/services/PedidoService.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import sistemaDeVendas.domains.Pedido;
-import sistemaDeVendas.domains.Produto;
 import sistemaDeVendas.repositorys.PedidoRepository;
 
 @Service
