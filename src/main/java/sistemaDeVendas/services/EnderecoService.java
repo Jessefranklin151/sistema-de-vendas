1e53f25fa28053f752986f9c99de8780268b7263
jesse <jesseoito@gmail.com>
2018-10-13 01:36:56 -0300

    arrumando um pequeno problema

--- src/main/java/sistemaDeVendas/services/EnderecoService.java
+++ src/main/java/sistemaDeVendas/services/EnderecoService.java
@@ -3,7 +3,6 @@ package sistemaDeVendas.services;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 
 import sistemaDeVendas.domains.Endereco;
@@ -14,34 +13,33 @@ public class EnderecoService {
 
 	@Autowired
 	private EnderecoRepository enderecoRepository;
-	
+
 	public void createEndereco(Endereco e) {
 		enderecoRepository.save(e);
 	}
-	
+
 	public void updateEndereco(Endereco e) {
 		Endereco enderecoBase = readEndereco(e);
-		
+
 		enderecoBase.setBairro(e.getBairro());
 		enderecoBase.setCidade(e.getCidade());
 		enderecoBase.setEstado(e.getEstado());
 		enderecoBase.setNumero(e.getNumero());
 		enderecoBase.setRua(e.getRua());
-		
+
 		enderecoRepository.save(enderecoBase);
 	}
 
 	public Endereco readEndereco(Endereco e) {
 		return enderecoRepository.findById(e.getId()).get();
 	}
-	
+
 	public ArrayList<Endereco> readAll() {
 		return (ArrayList<Endereco>) enderecoRepository.findAll();
 	}
-	
+
 	public void removeEndereco(Endereco e) {
 		enderecoRepository.deleteById(e.getId());
 	}
-	
-	
- }
+
+}
